3d5406c0f80e5525c004300c80db3d4a8f536cc7
Paweł Wanat <wanatpj@gmail.com>
2016-06-22 22:22:44 +0200

    Adds posibility to compare watermark against another watermark

--- compute_linear_correlation.py
+++ compute_linear_correlation.py
@@ -7,20 +7,33 @@ from PIL import Image
 from common import *
 
 def _parse_flags():
-  global indir, referencefile
+  global indir, inreferencefile, referencefile, versuswatermark
   parser = OptionParser()
   parser.add_option("-i",
       "--in",
       dest="indir",
       help="location to directory that containes images to watermark",
       metavar="DIR")
+  parser.add_option("-v",
+      "--inreference",
+      dest="inreference",
+      help="monochrome image; black pixel denotes 1, white pixel denotes -1",
+      metavar="FILE")
   parser.add_option("-w",
       "--reference",
       dest="reference",
       help="monochrome image; black pixel denotes 1, white pixel denotes -1",
       metavar="FILE")
   (options, args) = parser.parse_args()
-  indir = options.indir
+  if (options.indir != None and options.inreference != None) \
+      or (options.indir == None and options.inreference == None):
+    raise Exception("define: indir xor inreference")
+  if options.indir != None:
+    indir = options.indir
+    versuswatermark = False
+  elif options.inreference != None:
+    inreferencefile = options.inreference
+    versuswatermark = True
   referencefile = options.reference
 
 class ComputeLinearCorrelation:
@@ -33,12 +46,16 @@ class ComputeLinearCorrelation:
         self.reference)
 
 def main():
-  global indir, referencefile
+  global indir, inreferencefile, referencefile, versuswatermark
   _parse_flags()
   (width, height), reference = get_watermark(referencefile)
-  lcs = get_pool().map(
-      ComputeLinearCorrelation(reference, indir),
-      filter(ImageSizeFilter((width, height), indir), os.listdir(indir)))
+  if versuswatermark:
+    (inwidth, inheight), inreference = get_watermark(inreferencefile)
+    lcs = [linear_correlation(reference, inreference)]
+  else:
+    lcs = get_pool().map(
+        ComputeLinearCorrelation(reference, indir),
+        filter(ImageSizeFilter((width, height), indir), os.listdir(indir)))
   print "Mean: " + str(numpy.mean(lcs))
   print "Median: " + str(numpy.median(lcs))
   print "Variance: " + str(numpy.var(lcs))
