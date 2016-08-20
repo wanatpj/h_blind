fbbd0e3030da92cc45edb87a8986a88924aebb79
Paweł Wanat <wanatpj@gmail.com>
2016-08-20 14:09:10 +0200

    redirection to /dev/null

--- benchmark-time.sh
+++ benchmark-time.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for i in {1..100}
+for i in {1..25}
 do
-   time python break_adj.py --watermarked=in/ --deduced=deduced.bmp --size=2592x1944 >> bench.out 2>> bench.err
+   time python break_adj.py --watermarked=in/ --deduced=deduced.bmp --size=2592x1944 >> /dev/null 2>> /dev/null
    array=(in/*)
    rm "${array[0]}"
 done
