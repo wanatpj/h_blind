d9fe3c4dbbe883d45eb673f4d195100ce40a2b09
Paweł Wanat <wanatpj@gmail.com>
2016-08-27 15:38:40 +0200

    clean up

--- benchmark-time.sh
+++ benchmark-time.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {1..25}
+for i in {1..100}
 do
    time python break_adj.py --watermarked=in/ --deduced=deduced.bmp --size=2592x1944 >> /dev/null 2>> /dev/null
    array=(in/*)
